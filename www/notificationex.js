048a56ee79f9488b701b4c67a086f71b234ead70
Robert Wallstrom <robert@smithimage.com>
2014-01-06 00:49:24 +0100

    Rafactored the js into cordova 3 style

--- www/notificationex.js
+++ www/notificationex.js
@@ -1,26 +1,31 @@
+
+var argscheck = require('cordova/argscheck'),
+utils = require('cordova/utils'),
+exec = require('cordova/exec');
+
 var NotificationEx = function() {
 };
 
 // iPhone only
-NotificationEx.loadingStart = function(options) {
+NotificationEx.prototype.loadingStart = function(options) {
     cordovaRef.exec(null, null, "NotificationEx","loadingStart", [options]);
 };
 // iPhone only
-NotificationEx.loadingStop = function() {
+NotificationEx.prototype.loadingStop = function() {
     cordovaRef.exec(null, null, "NotificationEx","loadingStop", []);
 };
 
 /**
  * Start spinning the activity indicator on the statusbar
  */
-NotificationEx.activityStart = function() {
+NotificationEx.prototype.activityStart = function() {
     cordovaRef.exec(null, null, "NotificationEx", "activityStart", []);
 };
 
 /**
  * Stop spinning the activity indicator on the statusbar, if it's currently spinning
  */
-NotificationEx.activityStop = function() {
+NotificationEx.prototype.activityStop = function() {
     cordovaRef.exec(null, null, "NotificationEx", "activityStop", []);
 };
 
