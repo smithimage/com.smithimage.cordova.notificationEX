cde7c81a03a071a2af6de724afaa9b12cb12b4e0
Robert Wallstrom <robert@smithimage.com>
2014-01-05 17:50:44 +0100

    Rafactored the js into cordova 3 style

--- www/notificationex.js
+++ www/notificationex.js
@@ -1,57 +1,34 @@
-///////////////////
-(function() {
-///////////////////
+cordova.define("com.smithimage.cordova.notificationEX", function(require, exports, module) {
 
-// get local ref to global PhoneGap/Cordova/cordova object for exec function
-var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
+var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
 
-/**
- * This class provides extended access to notifications on the device. (iOS)
- * In Cordova.plist/Plugins, add this mapping (key:NotificationEx, value:NotificationEx)
- */
-NotificationEx = function() {
+var NotificationEx = function() {
 };
 
 // iPhone only
-NotificationEx.prototype.loadingStart = function(options) {
+NotificationEx.loadingStart = function(options) {
     cordovaRef.exec(null, null, "NotificationEx","loadingStart", [options]);
 };
 // iPhone only
-NotificationEx.prototype.loadingStop = function() {
+NotificationEx.loadingStop = function() {
     cordovaRef.exec(null, null, "NotificationEx","loadingStop", []);
 };
 
 /**
  * Start spinning the activity indicator on the statusbar
  */
-NotificationEx.prototype.activityStart = function() {
+NotificationEx.activityStart = function() {
     cordovaRef.exec(null, null, "NotificationEx", "activityStart", []);
 };
 
 /**
  * Stop spinning the activity indicator on the statusbar, if it's currently spinning
  */
-NotificationEx.prototype.activityStop = function() {
+NotificationEx.activityStop = function() {
     cordovaRef.exec(null, null, "NotificationEx", "activityStop", []);
 };
 
-NotificationEx.install = function() {
-    if (typeof navigator.notificationEx == "undefined") {
-		navigator.notificationEx = new NotificationEx();
-	}
-};
-
-/**
- * Add to Cordova constructor
- */
-if (cordovaRef && cordovaRef.addConstructor) {
-	cordovaRef.addConstructor(NotificationEx.install);
-} else {
-	console.log("NotificationEx Cordova Plugin could not be installed.");
-	return null;
-}
-
-
-///////////////////
-})();
-///////////////////
+module.exports = StatusBar;
+});
